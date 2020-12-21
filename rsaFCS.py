a40903a47deb4257d3708b7e3480b57cb06819dd
kyzylmonteiro <32486621+kyzylmonteiro@users.noreply.github.com>
2020-12-22 02:51:54 +0530

    byte conversion issues resolved

--- rsaFCS.py
+++ rsaFCS.py
@@ -12,8 +12,10 @@ from PIL import Image
 def read_msg(encrypted_text, public_key):
     # input: bytes; output: string
     # rsa_public_key = RSA.importKey(public_key)
+    print("encrypted text")
+    print(encrypted_text)
+    encrypted_text = bytes.fromhex(encrypted_text)
     rsa_public_key = PKCS1_OAEP.new(public_key)
-    encrypted_text = base64.b64decode(encrypted_text)
     decrypted_text = rsa_public_key.decrypt(encrypted_text)
 
     # print('your decrypted_text is : {}'.format(decrypted_text))
@@ -27,7 +29,8 @@ def gen_SHA256_digest(message):
     # return obj.hexdigest()  # string
     # print("hashlib.sha256(message).hexdigest()")
     # print(hashlib.sha256(message).hexdigest())
-    return hashlib.sha256(message).hexdigest()
+    hashop = hashlib.sha256(message).hexdigest()
+    return hashop
 
 
 def encode_msg(message, private_key):
@@ -47,7 +50,13 @@ def encode_msg(message, private_key):
     # print(encrypted_nof)
     # print("base64.b64encode(encrypted_nof)")
     # print(base64.b64encode(encrypted_nof))
-    return base64.b64encode(encrypted_nof)
+    ret_val = encrypted_nof.hex()
+    if(len(ret_val)%2!=0):
+        ret_val = '0' + ret_val
+    print("encrypted_nof.hex()")
+    print(len(ret_val))
+    print(ret_val)
+    return ret_val
 
 
 # def update_msg(encrypted_nof, public_key, private_key):
@@ -82,7 +91,7 @@ def extract_code(image):
     # print(arr[-1])
     # for i in range(len(whr[0])):
     #   if(whr[0][i]==whr[0][i+1] and whr[0][i+1]==whr[0][i+2] ):
-    if np.sum(arr[arr.shape[0]-1] == [0, 0, 0, 0]) != 4:
+    if np.sum(arr[arr.shape[0]-1] == [69, 69, 69, 69]) != 4:
         return False
     else:
         encoded_arr = arr.reshape(-1)
@@ -94,45 +103,53 @@ def extract_code(image):
         print(encoded_list)
         s = ''
         for i in encoded_list:
-            if(len(str(i)) < 2):
-                s = s + '0' + str(i)
-            else:
-                s = s + str(i)
+            # if(len(str(i)) < 2):
+            #     s = s + '0' + hex(i)[2:]
+            # else:
+            s = s + hex(i)[2:]
 
-        x = int(s)
+        # x = int(s)
 
         new_img_arr = img_arr[:index-1]
         new_img = Image.fromarray(new_img_arr)
         new_img.save("recovered.png", format="png")
 
-        print("x")
-        print(x)
-        encoded_hash = x.to_bytes((x.bit_length() + 7) // 8, byteorder='big')
-        print("encoded_hash")
-        print(type(encoded_hash))
-        print(encoded_hash)
+        # print("x")
+        # # print(x)
+        # # encoded_hash = x.to_bytes((x.bit_length() + 7) // 8, byteorder='big')
+        # print("encoded_hash")
+        # print(type(encoded_hash))
+        # print(encoded_hash)
 
-        return encoded_hash
+        # encoded_hash = encoded_hash.hex()
+        # encoded_hash is now a hex string
+        print("extractd_code")
+        print(s)
+        return s
 
 
 # c = b"\xc6\xf5\xf8\xb8\xcd2\x10M\x84\x7f\x86\xaa3\x86\xbf_\xa7\x058\x0e.\x94\xfe\x08\xa5\xcbA\x0b\x82\x88\xd8\x8e\xc6w\xf3\xe4\xb4\xa6F\xe90\xb5\x0b\xfb\xe2@z\x1d\x93P\x1fZZ\x8fW%\x14\xcd!\xe6\xb2\x93\xd5Ou\xae'y\x8f\xf8\xcf\xf9\x17=S\x18\xe1\xcfH 2\xba\xe2\xb5-y\xd9\x07\x98\x94\xad7f\xaf\xf7Q\x91P\x06o,\xd6\x94\xa4\x94\xc0+\xa0Hd\xed\xe4\x9c\xda\xacg\x88crEY\x94\xbe\xe4@<\xa5`H#\xd1<\xbab5\x18\xc0\x9a(]\x14\xc7]r\xf9\xcb\x91\t\x1e0_\xc2o\xb8\x9d\xe8\xe2\x8a2\xee\xdb\x99\x9e\xba@;>\xf8\x07\x1e?\xa8\xea\x0e\xb1\xbb\xa1\x91a\xa9\xd8:\r\x1e\xdd}\x1fT\xc7,*\x9aV9F\x12M\xed\xd0e\xb7\xd7D\x89nbs\xb3\x95L]\xbc\x04\x1a\x88<\x01\xe6\xe5<\x00\x0c\xcf.\x9e7&.\x89\x1c\xc2\xc1\xb4\xd2\xa8\xcf\xde\xd6\xcas\xbb\xa2-#~s$0\xcb\x0cV\xbck\xf7;?"
 def append2img(c, image):
+    # c is a string with hex values
     # print("c")
     # print(c)
     # c=b'R0+kdh3W0aia6cMQk4TEi9Q8rHEFCiaK+C21Q57GTf+IPDAKoOr8kQlHwaPa9ELeGiP5ksyz6lIMjJf9BxdJf/GBvw17K+y1KdJuhaiGJCGS9baFgwdSRZ+OjdsAt5xyGrev8j2h8kbDQWG0sqFjz1jCZZfUX+12bOPUZPtcPMDW3T2gvcCrlTvsZldmh4kHFetD2lyOd4+kHpe5SLsgTaHLhHyw2a74wxF13b7Ksf8mITMlSNtbsnVvMdJ5AhgbGPGy5ntT7OAOksJ7HCpo74vmXrXMYU9iedl663BxuecC9YXEP2SE/QK9O9eSwP43R1O9NsI35AmC1TYzTGodUg=='
-    a = int.from_bytes(c, byteorder='big', signed=False)
-
-    s = str(a)
+    # c = bytes.fromhex(c)
+    # a = int.from_bytes(c, byteorder='big', signed=False)
+    print("c")
+    print(c)
+    s = c
+    # s = str(a)
     arr = []
     temp = []
-    chunks = [s[i:i+2] for i in range(0, len(s), 2)]
+    chunks = [s[i:i+1] for i in range(0, len(s), 1)]
     for i in range(0, len(chunks)):
         if((i+1) % 4 == 0):
-            temp.append(int(chunks[i]))
+            temp.append(int(chunks[i], 16))
             arr.append(temp)
             temp = []
         else:
-            temp.append(int(chunks[i]))
+            temp.append(int(chunks[i], 16))
 
     if(len(temp) != 0):
         for i in range(4-len(temp)):
@@ -153,9 +170,9 @@ def append2img(c, image):
     t = np.array([arr], dtype=np.uint8)
     img = Image.open(image)
     img_arr = np.array(img)
-    tt = np.zeros(
+    tt = np.ones(
         (1, int(np.shape(img_arr[1, :])[0] - np.shape(t)[1]), 4), dtype=np.uint8)
-
+    tt = tt * 69
     key_arr = np.concatenate((t, tt), axis=1)
     new_img_arr = np.concatenate((img_arr, key_arr), axis=0)
 
